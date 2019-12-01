d96a32d4fb562a07647d0d7c1652782025119c18
ABOUADANE El Mehdi <hidou-abou@live.fr>
2019-12-01 22:15:07 +0100

    Part 4

--- src/main/java/org/sid/SecServiceApplication.java
+++ src/main/java/org/sid/SecServiceApplication.java
@@ -1,7 +1,14 @@
 package org.sid;
 
+import org.sid.entities.AppRole;
+import org.sid.service.AccountService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class SecServiceApplication {
@@ -10,4 +17,20 @@ public class SecServiceApplication {
         SpringApplication.run(SecServiceApplication.class, args);
     }
 
+    @Bean
+    CommandLineRunner start(AccountService accountService) {
+        return args -> {
+            accountService.save(new AppRole(null, "USER"));
+            accountService.save(new AppRole(null, "ADMIN"));
+            Stream.of("user1","user2","user3","admin").forEach(u -> {
+                accountService.saveUser(u, "1234", "1234");
+            });
+            accountService.addRoleToUser("admin", "ADMIN");
+        };
+    }
+
+    @Bean
+    BCryptPasswordEncoder getBCPE() {
+        return new BCryptPasswordEncoder();
+    }
 }
